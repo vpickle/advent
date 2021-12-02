4aff0076a21abf1423bc58cb7e2c943c2af187d5
vpickle <95370535+vpickle@users.noreply.github.com>
2021-12-02 16:20:19 -0500

    Add files via upload

--- day2_1.py
+++ day2_1.py
@@ -11,3 +11,21 @@ for line in open('day2_input.txt'):
         depth -= value
 
 print(horz*depth)
+
+#second question
+
+horz = 0 #horizontal position
+depth = 0 #depth position
+aim = 0 #aim
+for line in open('day2_input.txt'):
+    action , value = line.split() # split the line into the action and the value
+    value = int(value) #convert to int
+    if action == 'forward':
+        horz += value
+        depth += aim*value
+    if action == 'down':
+        aim += value
+    if action == 'up':
+        aim -= value
+
+print(horz*depth)
